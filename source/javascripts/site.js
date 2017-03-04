135711c8b2ba1c3b0d199a8eee6848c62b288f1e
JustinBrown <jb@justinbrown.io>
2017-03-04 00:44:06 -0700

    top-bar and footer

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,1 +1,10 @@
 // This is where it all goes :)
+// http://stackoverflow.com/questions/11434735/change-element-style-on-page-scroll#11434786
+// var win = window,
+//     docEl = document.documentElement,
+//     $topBar = document.getElementById('top-bar-container');
+
+// win.onscroll = function(){
+//    var sTop = (this.pageYOffset || docEl.scrollTop)  - (docEl.clientTop || 0);
+//    $topBar.style.backgroundColor =  sTop > 100 ? "white":"transparent" ;
+// };
