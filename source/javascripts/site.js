8cd21be5a86405783676ff02a5abc41f9bb12b69
JustinBrown <jb@justinbrown.io>
2017-03-05 18:31:07 -0700

    Top bar scroll effect and transition.

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -24,19 +24,32 @@
 $(window).scroll(function() {
   var viewableOffset = $("html").offset().top - $(window).scrollTop();
   var topBar = $('#top-bar-container');
+  // var topBarHr = $('#top-bar-hr');
   console.log(viewableOffset);
   if (viewableOffset < -70) {
     var styles = {
-        backgroundColor: '#fbf9f1',
+        // backgroundColor: '#fbf9f1',
+        backgroundColor: '#fffefd',
         height: '80',
         borderBottom: 'none'
+        // borderBottom: 'solid 1px rgba(0,0,0,0.1)'
     };
+    // var hrStyles = {
+    //   borderTop: 'none',
+    //   borderBottom: 'none'
+    // };
   } else {
     var styles = {
         backgroundColor: '#F7F2E1',
         height: '120',
+        // borderBottom: 'none'
         borderBottom: 'solid 1px rgba(0,0,0,0.2)'
     };
+    // var hrStyles = {
+    //   borderTop: '1px solid rgba(0, 0, 0, 0.2)',
+    //   borderBottom: '1px solid rgba(255, 255, 255, 0.3)'
+    // };
   };
   topBar.css( styles );
+  // topBarHr.css( hrStyles );
 });
