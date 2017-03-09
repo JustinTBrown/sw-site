a4a1ef1d03ba759c228cfcf4cdab8d326860fde8
JustinBrown <jb@justinbrown.io>
2017-03-08 21:17:17 -0700

    top-bar transitions

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,42 +1,14 @@
-// This is where it all goes :)
-// http://stackoverflow.com/questions/11434735/change-element-style-on-page-scroll#11434786
-// var win = window,
-//     docEl = document.documentElement,
-//     $topBar = document.getElementById('top-bar-container');
-
-// win.onscroll = function(){
-//    var sTop = (this.pageYOffset || docEl.scrollTop)  - (docEl.clientTop || 0);
-//    $topBar.style.backgroundColor =  sTop > 100 ? "white":"transparent" ;
-// };
-
-// var targetOffset = $("#anchor-point").offset().top;
-
-// var $w = $(window).scroll(function(){
-//     if ( $w.scrollTop() > targetOffset ) {   
-//         $('#voice2').css({"border-bottom":"2px solid #f4f5f8"});
-//         $('#voice3').css({"border-bottom":"2px solid #2e375b"});
-//     } else {
-//       // ...
-//     }
-// });
-
-
 $(window).scroll(function() {
   var viewableOffset = $("html").offset().top - $(window).scrollTop();
   var nav = $('.navigation');
-  // var navWrap = $('.navigation-wrapper ');
   var navHr = $('#navHr');
   console.log(viewableOffset);
   if (viewableOffset < -70) {
     var navStyles = {
         backgroundColor: '#fffefd',
-        // borderBottom: 'none'
-        // borderBottom: 'solid 1px rgba(0,0,0,0.1)'
     };
     var navWrapStyles = {
-      // borderBottom: 'none'
     };
-    // navHr.hide();
     var navHrStyles = {
       borderTop: 'none',
       borderBottom: 'none'
@@ -45,14 +17,9 @@ $(window).scroll(function() {
   } else {
     var navStyles = {
       backgroundColor: '#F7F2E1',
-      // borderBottom: 'solid 1px rgba(0,0,0,0.2)'
-        // borderBottom: 'none'
     };
     var navWrapStyles = {
-      // borderBottom: 'solid 1px rgba(0,0,0,0.2)'
-      // borderBottom: 'none'
     };
-    // navHr.show();
     var navHrStyles = {
       borderTop: '1px solid rgba(0, 0, 0, 0.2)',
       borderBottom: '1px solid rgba(255, 255, 255, 0.3)'
@@ -60,7 +27,6 @@ $(window).scroll(function() {
     nav.removeClass('scrolled');
   };
   nav.css( navStyles );
-  // navWrap.css( navWrapStyles );
   navHr.css( navHrStyles );
 });
 
@@ -70,25 +36,30 @@ $(document).ready(function() {
 
   menuToggle.on('click', function(e) {
     e.preventDefault();
-    $('#js-navigation-menu').slideToggle(function(){
+
+    if($('#js-navigation-menu').is(':hidden')) {
+      $('#overlay').css({
+        backgroundColor: 'rgba(0,0,0,0.2)',
+      });
+    } else {
+      $('#overlay').css({
+        backgroundColor: 'rgba(1,1,1,0.0)',
+      });
+    }
+    
+    $('#js-navigation-menu').slideToggle("300",function(){
       if($('#js-navigation-menu').is(':hidden')) {
         $('#js-navigation-menu').removeAttr('style');
-        $('#overlay').css({
-          backgroundColor: 'rgba(1,1,1,0.0)',
-        });
+        // $('#overlay').css({
+        //   backgroundColor: 'rgba(1,1,1,0.0)',
+        // });
         if($('.navigation').hasClass('scrolled')) {
-
         } else {
           $('#navHr').show();
-          
         }
       } else {
-        $('#overlay').css({
-          backgroundColor: 'rgba(0,0,0,0.2)',
-        });
-        // $('#navHr').css({
-        //   borderTop: 'none',
-        //   borderBottom: 'none'
+        // $('#overlay').css({
+        //   backgroundColor: 'rgba(0,0,0,0.2)',
         // });
         $('#navHr').hide();
       }
